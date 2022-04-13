22b6cf85358b98432cad17a113380b83d120378c
dk-Gk <idkin19@gmail.com>
2022-04-13 20:13:19 +0000

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Python script that, using this REST API, for a given employee ID,
- returns information about his/her TODO list progress."""
+"""Python script"""
 
 
 if __name__ == "__main__":
